d0304fb39805ea5af1f833420e1624092a64aeee
Sergey Somkov <sergey@somkov.com>
2021-09-29 06:54:13 +0000

    #8 - dotnet build diagnostic

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -2,7 +2,7 @@ set -e
 set -u
 
 dotnet restore --configfile NuGet.Config
-dotnet build --configuration Release --no-restore /p:Version=${SERVICE_VERSION} /p:RepositoryUrl=${SERVICE_REPOSITORY_URL}
+dotnet build --verbosity diagnostic --configuration Release --no-restore /p:Version=${SERVICE_VERSION} /p:RepositoryUrl=${SERVICE_REPOSITORY_URL}
 dotnet test --configuration Release --no-build --verbosity normal
 
 dotnet nuget push **/*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --no-symbols true
