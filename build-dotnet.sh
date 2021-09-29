556d9915a1e015f1a92d5b053d041e10e41de381
Sergey Somkov <sergey@somkov.com>
2021-09-29 12:35:39 +0300

    #8 - build fix desperate try (1)

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -2,7 +2,10 @@ set -e
 set -u
 
 dotnet restore --configfile NuGet.Config
-dotnet build --verbosity diagnostic --configuration Release --no-restore /p:Version=${SERVICE_VERSION} /p:RepositoryUrl=${SERVICE_REPOSITORY_URL}
+cd azure-functions-host
+dotnet build --configuration Release --no-restore /p:Version=${SERVICE_VERSION} /p:RepositoryUrl=${SERVICE_REPOSITORY_URL} src\WebJobs.Script.WebHost\WebJobs.Script.WebHost.csproj
+cd ..
+dotnet build --configuration Release --no-restore /p:Version=${SERVICE_VERSION} /p:RepositoryUrl=${SERVICE_REPOSITORY_URL}
 dotnet test --configuration Release --no-build --verbosity normal
 
 dotnet nuget push **/*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --no-symbols true
