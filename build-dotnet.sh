558b6ac0c0462000411737a487a0f32d9b40bbc1
Sergey Somkov <sergey@somkov.com>
2021-10-04 21:55:35 +0300

    #9 - build fix: dotnet restore --disable-parallel

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -4,7 +4,7 @@ set -u
 FUNCTION_PROJECT=StatusPage.Function
 FUNCTION_PATH=${FUNCTION_PROJECT}/out/${SERVICE_CONFIGURATION}
 
-dotnet restore --configfile NuGet.Config
+dotnet restore --configfile NuGet.Config --disable-parallel
 dotnet build --configuration ${SERVICE_CONFIGURATION} --no-restore /p:Version=${SERVICE_VERSION} /p:RepositoryUrl=${SERVICE_REPOSITORY_URL}
 dotnet publish --configuration ${SERVICE_CONFIGURATION} --no-build --output ${FUNCTION_PATH} ${FUNCTION_PROJECT}/${FUNCTION_PROJECT}.csproj
 dotnet test --configuration ${SERVICE_CONFIGURATION} --no-build --verbosity normal
