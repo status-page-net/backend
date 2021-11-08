b9d38a9eadd6507d94f9ed4dfe5e6b9e87430cd4
Sergey Somkov <sergey@somkov.com>
2021-11-08 03:10:18 +0300

    #20 - Server logging configuration

--- StatusPage.Server/Startup.cs
+++ StatusPage.Server/Startup.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MongoDB.Driver;
+using Microsoft.Extensions.Logging;
 using StatusPage.BLL;
 using StatusPage.MongoDB;
 
@@ -37,6 +37,14 @@ namespace StatusPage.Server
 
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddLogging((ILoggingBuilder builder) =>
+			{
+				builder.ClearProviders();
+				builder.AddConfiguration(
+					_configuration.GetSection("Logging"));
+				builder.AddConsole();
+			});
+
 			services.AddControllers();
 
 			services.AddStatusPageBLL();
